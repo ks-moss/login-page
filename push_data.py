9bc78c1ae696658634e2688350236629feadeb2f
ks-moss <k.sinamkam.usa@gmail.com>
2023-10-01 18:03:38 -0700

    Update

--- push_data.py
+++ push_data.py
@@ -2,11 +2,14 @@ import os
 from github import Github
 
 # GitHub repository information
-repository_owner = "ks-moss"
+repository_owner = "ks_moss"
 repository_name = "login-page"
 access_token = "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"
 
-# Paths to the database files
+# Directory where the database files are located
+database_directory = "database/"
+
+# List of database file names
 db_files = ["user_data.db", "user_credentials.db", "password_key.db"]
 
 # Create a GitHub instance
@@ -19,24 +22,14 @@ repo = github.get_user(repository_owner).get_repo(repository_name)
 branch = repo.get_branch("main")  # Use the appropriate branch name
 
 for db_file in db_files:
-    with open(db_file, "rb") as file:
+    # Construct the full path to the database file
+    db_file_path = os.path.join(database_directory, db_file)
+    
+    with open(db_file_path, "rb") as file:
         content = file.read()
     
     # Commit the file to the repository
-    repo.create_file(db_file, f"Committing {db_file}", content, branch=branch)
-
-# Now, you can retrieve the files when needed
-# For example, to fetch the latest version of 'user_data.db':
-
-latest_commit = repo.get_commits()[0]
-tree = latest_commit.commit.tree
-
-for db_file in db_files:
-    if db_file in [item.path for item in tree]:
-        # Download the file
-        file_content = repo.get_contents(db_file, ref=branch)
-        with open(db_file, "wb") as file:
-            file.write(file_content.decoded_content)
+    repo.create_file(db_file_path, f"Committing {db_file}", content, branch=branch)
 
 print("UPDATED!!!!!!!!!")
 
