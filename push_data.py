641fdf83fb6ee9a1f839fac29cea699572c26521
ks-moss <k.sinamkam.usa@gmail.com>
2023-10-01 18:34:30 -0700

    Update

--- push_data.py
+++ push_data.py
@@ -1,11 +1,10 @@
-import os
 from github import Github
+import os
 
 # GitHub repository information
-repository_owner = "ks-moss"  # Replace with the organization or user
-repository_name = "login-page"  # Replace with the repository name
-username = "ks-moss"  # Replace with your GitHub username
-password = "Moss_ks21"  # Replace with your GitHub password
+repository_owner = "ks_moss"
+repository_name = "login-page"
+access_token = "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"
 
 # Directory where the database files are located
 database_directory = "database/"
@@ -13,29 +12,27 @@ database_directory = "database/"
 # List of database file names
 db_files = ["user_data.db", "user_credentials.db", "password_key.db"]
 
-try:
-    # Create a GitHub instance with username and password
-    github = Github(username, password)
+# Create a GitHub instance using the access token
+g = Github(access_token)
 
-    # Get the repository without lazy loading
-    repo = github.get_repo(f"{repository_owner}/{repository_name}", lazy=False)
+# Get the specified repository
+repo = g.get_user(repository_owner).get_repo(repository_name)
 
-    # Commit and push the database files
-    branch = repo.get_branch("main")  # Use the appropriate branch name
+print("REPO: ", repo)
 
-    for db_file in db_files:
-        # Construct the full path to the database file
-        db_file_path = os.path.join(database_directory, db_file)
-
-        with open(db_file_path, "rb") as file:
-            content = file.read()
-
-        # Commit the file to the repository
-        repo.create_file(db_file_path, f"Committing {db_file}", content, branch=branch)
-
-    print("UPDATED!!!!!!!!!")
-
-    # Now, you can access the data in 'user_data.db' and other database files
-    # and perform the necessary operations.
-except Exception as e:
-    print(f"An error occurred: {e}")
+# Iterate through the database files and push them to the repository
+for db_file in db_files:
+    file_path = os.path.join(database_directory, db_file)
+    with open(file_path, 'rb') as file:
+        content = file.read()
+    try:
+        # Create or update the file in the repository
+        repo.create_file(
+            path=file_path,
+            message=f"Update {db_file}",
+            content=content,
+            branch="main"  # Change this branch name if necessary
+        )
+        print(f"{db_file} has been successfully pushed to the repository.")
+    except Exception as e:
+        print(f"Error occurred while pushing {db_file}: {str(e)}")
