10ba37b52bb9426f82b13675a8d78ab577c998de
ks-moss <k.sinamkam.usa@gmail.com>
2023-10-01 19:15:16 -0700

    Update

--- database/push_data.py
+++ database/push_data.py
@@ -17,11 +17,11 @@ g = Github(access_token)
 
 # Get the specified repository
 repo = g.get_repo(f"{repository_owner}/{repository_name}")
-    
 
 # Iterate through the database files and push them to the repository
 for db_file in db_files:
     file_path = os.path.join(database_directory, db_file)
+    print("PATH: ", file_path)
     with open(file_path, 'rb') as file:
         content = file.read()
     
